59fade6ff8add2dc5b03feb59917a28d2952bd36
kgurchiek <90143063+kgurchiek@users.noreply.github.com>
2023-01-07 22:27:40 -0800

    Update scan.js

--- scan.js
+++ scan.js
@@ -2,9 +2,9 @@ const { MinecraftServerListPing } = require("minecraft-status");
 const fs = require("fs");
 const ipList = require("./masscan.json");
 var file = require("./serverList.json");
-const pingChunkSize = 10000;
+const pingChunkSize = 5000;
 const pingTimeout = 3000;
-const chunkCap = 50;
+const chunkCap = 100;
 var chunksScanned = 0;
 var successes = [];
 
