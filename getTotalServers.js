9e8b758b33bf72830c11bd71abdf59f45dabdde1
kgurchiek <90143063+kgurchiek@users.noreply.github.com>
2023-02-07 07:40:47 -0800

    Update getTotalServers.js

--- getTotalServers.js
+++ getTotalServers.js
@@ -1,5 +1,5 @@
 const file = require("./serverList.json");
 const ipList = require("./masscan.json");
 
-console.log("masscan found " + ipList.length + " total servers");
+console.log("masscan found " + ipList.servers.length + " total servers");
 console.log(file.totalServers + " total servers have been confirmed");
